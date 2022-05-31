33bc4b4765b50c0fe8350b56393bd6f4c4d1151c
Your Name <you@example.com>
2022-05-31 15:33:33 -0700

    random password generated

--- src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -6,19 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
 import org.springframework.beans.BeanUtils;
+import com.appsdeveloperblog.app.ws.shared.Utils;
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    Utils utils;
+
     @Override
     public UserDto createUser(UserDto user){
+
+        if(userRepository.findByEmail(user.getEmail()) != null) throw new RuntimeException("Record already exists");
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(user, userEntity);
 
+        String publicUserId = utils.generateUserId(30);
+        userEntity.setUserId(publicUserId);
         userEntity.setEncryptedPassword("test");
-        userEntity.setUserId("testUserId");
         UserEntity storedUserDetails = userRepository.save(userEntity);
         UserDto returnValue = new UserDto();
         BeanUtils.copyProperties(storedUserDetails, returnValue);
