c40057cbb7a49913ce21fd19852907f9a064213a
Your Name <you@example.com>
2022-05-31 13:05:10 -0700

    works with email verification status commented out

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -1,24 +1,38 @@
 package com.appsdeveloperblog.app.ws.ui.controller;
 
+import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
-
+import org.springframework.web.bind.annotation.RequestBody;
+import com.appsdeveloperblog.app.ws.ui.model.request.UserDetailsRequestModel;
+import org.springframework.beans.BeanUtils;
+import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
+import com.appsdeveloperblog.app.ws.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 @RestController
 @RequestMapping("users")
 public class UserController {
 
+    @Autowired
+    UserService userService;
     @GetMapping
     public String getUser(){
         return "get user was called";
     }
 
     @PostMapping
-    public String createUser(){
-        return "create user was called";
+    public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails){
+        UserRest returnValue = new UserRest();
+        UserDto userDto = new UserDto();
+        BeanUtils.copyProperties(userDetails, userDto);
+        UserDto createdUser = userService.createUser(userDto);
+        BeanUtils.copyProperties(createdUser, returnValue);
+        return returnValue;
+
     }
 
     @PutMapping
