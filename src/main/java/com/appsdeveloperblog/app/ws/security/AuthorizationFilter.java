596eff6122d5e9868a9f6a10c86df92610ff57e4
Your Name <you@example.com>
2022-06-02 16:49:01 -0700

    next session coded

---
+++ src/main/java/com/appsdeveloperblog/app/ws/security/AuthorizationFilter.java
@@ -0,0 +1,56 @@
+package com.appsdeveloperblog.app.ws.security;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import javax.servlet.FilterChain;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import io.jsonwebtoken.Jwts;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AuthorizationFilter extends BasicAuthenticationFilter {
+
+    public AuthorizationFilter(AuthenticationManager authManager) {
+        super(authManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req,
+                                    HttpServletResponse res,
+                                    FilterChain chain) throws IOException, ServletException {
+        String header = req.getHeader(SecurityConstants.HEADER_STRING);
+        if (header == null || !header.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+            chain.doFilter(req, res);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(req);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(req, res);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+        String token = request.getHeader(SecurityConstants.HEADER_STRING);
+
+        if (token != null) {
+            token = token.replace(SecurityConstants.TOKEN_PREFIX, "");
+            String user = Jwts.parser()
+                    .setSigningKey(SecurityConstants.TOKEN_SECRET)
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getSubject();
+
+            if (user != null) {
+                return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+            }
+            return null;
+        }
+        return null;
+    }
+}
+
+
